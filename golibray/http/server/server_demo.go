69dc2446715f4308e6249d81f66acf1cb1b58268
XianCH <1160698903@qq.com>
2023-09-12 07:04:46 +0800

    freamworkstu

--- golibray/http/server/server_demo.go
+++ golibray/http/server/server_demo.go
@@ -10,12 +10,10 @@ func main() {
 	http.ListenAndServe("127.0.0.1:8000", nil)
 
 	s := &http.Server{
-		Addr: "127.0.0.1:8000",
+		Addr:    "127.0.0.1:8000",
 		Handler: fooHandler(),
 	}
 }
-
-
 
 func fooHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.RemoteAddr, "成功连接")
