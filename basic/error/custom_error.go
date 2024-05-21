566d27d9219fddaa17b785ebf6fc6c3f6de9e26d
XianCH <x14n911@gmail.com>
2024-05-21 23:59:17 +0800

    update custom error

--- basic/error/custom_error.go
+++ basic/error/custom_error.go
@@ -1,8 +1,21 @@
 package errortest
 
+import (
+	"encoding/json"
+	"runtime"
+)
+
+// Error 实现了自定义错误接口
+type customError struct {
+	msg     string
+	code    int
+	callers []CallerInfo
+	wrapped []error
+}
+
 type Error interface {
 	Caller() []CallerInfo
-	Wraped() []error
+	Wrapped() []error
 	Code() int
 	error
 
@@ -15,11 +28,112 @@ type CallerInfo struct {
 	FileLine int
 }
 
-func New(msg string) error
-func NewWithCode(code int, msg string) error
+func (e *customError) Error() string {
+	return e.msg
+}
 
-func Wrap(err error, msg string) error
-func WrapWithCode(code int, err error, msg string) error
+func (e *customError) Caller() []CallerInfo {
+	return e.callers
+}
 
-func FromJson(json string) (Error, error)
-func ToJson(err error) string
+func (e *customError) Wrapped() []error {
+	return e.wrapped
+}
+
+func (e *customError) Code() int {
+	return e.code
+}
+
+func (e *customError) private() {}
+
+//	func captureCallers() []CallerInfo {
+//		pc := make([]uintptr, 10)
+//		n := runtime.Callers(2, pc)
+//		frames := runtime.CallersFrames(pc[:n])
+//		var callers []CallerInfo
+//
+//		for {
+//			frame, more := frames.Next()
+//			callers = append(callers, CallerInfo{
+//				FuncName: frame.Function,
+//				FileName: frame.File,
+//				FileLine: frame.Line,
+//			})
+//			if !more {
+//				break
+//			}
+//		}
+//		return callers
+//	}
+func captureCallers() []CallerInfo {
+	pc := make([]uintptr, 10)
+	n := runtime.Callers(3, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	var callers []CallerInfo
+
+	for {
+		frame, more := frames.Next()
+		if frame.Function == "runtime.goexit" {
+			break
+		}
+		callers = append(callers, CallerInfo{
+			FuncName: frame.Function,
+			FileName: frame.File,
+			FileLine: frame.Line,
+		})
+		if !more {
+			break
+		}
+	}
+	return callers
+}
+
+func New(msg string) error {
+	return &customError{
+		msg:     msg,
+		callers: captureCallers(),
+	}
+}
+
+func NewWithCode(code int, msg string) error {
+	return &customError{
+		msg:     msg,
+		code:    code,
+		callers: captureCallers(),
+	}
+}
+
+func Wrap(err error, msg string) error {
+	return &customError{
+		msg:     msg,
+		callers: captureCallers(),
+		wrapped: []error{err},
+	}
+}
+
+func WrapWithCode(code int, err error, msg string) error {
+	return &customError{
+		msg:     msg,
+		code:    code,
+		callers: captureCallers(),
+		wrapped: []error{err},
+	}
+}
+
+func FromJson(jsonStr string) (Error, error) {
+	var e customError
+	err := json.Unmarshal([]byte(jsonStr), &e)
+	if err != nil {
+		return nil, err
+	}
+	return &e, nil
+}
+
+func ToJson(err error) string {
+	e, ok := err.(*customError)
+	if !ok {
+		return ""
+	}
+	jsonStr, _ := json.Marshal(e)
+	return string(jsonStr)
+}
